95a7a6ab11756f3ce8ab831b7c12fa02c2e9fe82
Bishop <dommennic@gmail.com>
2020-09-16 19:19:35 +0300

    Initial commit. Project structure.

---
+++ application/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_migrate import Migrate
+
+migrate = Migrate()
+
+
+def create_app():
+    """
+    Create and configure an instance of the Flask application.
+    """
+    app = Flask(__name__)
+    # Config
+    from application.config import Config
+    app.config.from_object(Config)
+    # SQLAlchemy
+    from application.models import db
+    db.init_app(app)
+    # Migrate.
+    migrate.init_app(app, db)
+
+    return app
