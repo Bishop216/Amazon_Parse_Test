1bd2368405ec880241b2a1286bd40da60794c17e
Bishop <dommennic@gmail.com>
2020-09-17 14:05:28 +0300

    Database config. SQLAlchemy models

--- application/config.py
+++ application/config.py
@@ -1,4 +1,20 @@
+try:
+    import application.local as local
+except ImportError:
+    raise RuntimeError("Database credentials are not provided!")
+
+
 class Config:
     """
     Database configuration.
     """
+    SQLALCHEMY_DATABASE_URI = (
+        "postgresql://{login}:{pwd}@{db_host}:{db_port}/{db_name}"
+        .format(
+            login=local.login,
+            pwd=local.password,
+            db_host=local.database_host,
+            db_port=local.database_port,
+            db_name=local.database_name
+        )
+    )
--- application/models.py
+++ application/models.py
@@ -1,3 +1,30 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+
+class Asins(db.Model):
+    __tablename__ = 'asins'
+
+    id = db.Column(db.Integer, nullable=False, unique=True, primary_key=True)
+
+
+class ProductInfo(db.Model):
+    __tablename__ = 'product_info'
+
+    id = db.Column(db.Integer, primary_key=True)
+    asin_id = db.Column(db.ForeignKey('asins.id', ondelete='CASCADE'))
+    name = db.Column(db.Text, nullable=False)
+    ratings = db.Column(db.Integer, nullable=False, default=0)
+    average_rating = db.Column(db.Numeric(1, 1), default=0)
+
+
+class Reviews(db.Model):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    asin_id = db.Column(db.ForeignKey('asins.id', ondelete='CASCADE'))
+    reviews_number = db.Column(db.Integer, nullable=False, default=0)
+    positive_reviews_number = db.Column(db.Integer, nullable=False, default=0)
+    critical_reviews_number = db.Column(db.Integer, nullable=False, default=0)
+    answered_auestions_number = db.Column(db.Integer, nullable=False, default=0)
